6c8a28c583a10a03c4e9c8923b3a5def8bffd4ba
Renato Andrade <renatoandrade7c@gmail.com>
2024-07-24 08:24:41 -0300

    Interligações de classes

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,13 @@
     //import Cond from './componentes/condicional.vue';
     //import Laco from './componentes/laco.vue';
     //import HTML from './componentes/html.vue';
-    import MODEL from './componentes/model.vue';
+    //import MODEL from './componentes/model.vue';
+    import CLS from './componentes/classe.vue';
+
 
 </script>
 
 
 <template>
-    <MODEL></MODEL>
+    <CLS></CLS>
 </template>
---
+++ src/componentes/classe.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+    let validacao = false;
+   
+
+</script>
+
+<style>
+
+    .ok{color: green;}
+    .falha{color: red;}
+
+</style>
+
+<template>
+
+<p :class="validacao? 'ok' : 'falha'">Utilizando diretiva class</p>
+    
+</template>

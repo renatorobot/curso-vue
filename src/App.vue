7eeb9a713595c7bdd837b0479b3d7d21ee2e2f2c
Renato Andrade <renatoandrade7c@gmail.com>
2024-07-24 08:53:52 -0300

    Evento de submissão

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,13 @@
     //import MODEL from './componentes/model.vue';
     //import CLS from './componentes/classe.vue';
     //import CLIQUE from './componentes/clique.vue';
-    import TECLADO from './componentes/teclado.vue'
+    //import TECLADO from './componentes/teclado.vue';
+    import SUBMISSAO from './componentes/submissao.vue'
 
 
 </script>
 
 
 <template>
-    <TECLADO></TECLADO>
+    <SUBMISSAO></SUBMISSAO>
 </template>
---
+++ src/componentes/submissao.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+    function validar(){
+        alert('formulario enviado');
+    }
+
+
+</script>
+
+
+
+<template>
+
+    <form @submit="validar">
+        <input type="submit" value="Enviar Formulario">
+    </form>
+    
+</template>
